062ac28e72fa8d7e56debc7c10381b394b904004
[Mathew Docker] <[mathew.docker@gmail.com]>
2018-10-04 15:51:37 +1000

    Add video list functionallity

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDwl2gNRGqmqErhEzSaGKmOuHo2rGgsAQE';
 YTSearch({key: API_KEY, term:'surfboards'}, 
@@ -24,6 +25,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		)
 	}
